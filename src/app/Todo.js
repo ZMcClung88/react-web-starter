6e38b1857bd121e400f46905a736288011242271
Zac McClung <zac.mcclung370@gmail.com>
2017-11-17 21:47:08 -0700

    initial commit

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 
+
 export class Todo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      nums: [1,2,3,4]
+    }
+  }
+  handleClick(e) {
+    console.log('there');
+    const nums = [...this.state.nums, 6];
+
+    this.setState({nums})
+  }
+
   render() {
     return (
-      <div>hello</div>
-    );
+      <div>
+        <input type="text" />
+        <button onClick={this.handleClick.bind(this)}>click</button>
+        {this.state.nums.map(num => (<h3 key={num}>{num}</h3>))}
+      </div>
+    )
   }
 }
